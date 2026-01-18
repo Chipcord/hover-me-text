90e9708f450c3576b0aee5e295cb57d820ea8b47
Chip <133388324+Chipcord@users.noreply.github.com>
2026-01-18 18:13:14 +0100

    Init

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen max-w-4xl mx-auto">
+      <div className="text-center grid grid-cols-3 gap-8 mt-[50vh]">
+        <p>hover me</p>
+        <p>hover me</p>
+        <p>hover me</p>
+        <p>hover me</p>
+        <p>hover me</p>
+        <p>hover me</p>
+      </div>
+    </main>
+  );
+}
